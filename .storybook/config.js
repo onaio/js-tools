a3eb857e9e70348ec49eee22a26ad5feae11e619
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-05 15:38:43 +0300

    Install Notes addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure } from '@storybook/react';
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js|jsx|ts|tsx}$/);
 function loadStories() {
+
   req.keys().forEach(filename => req(filename));
 }
-
 configure(loadStories, module);
