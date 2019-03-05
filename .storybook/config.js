a7e1706f6f0ed6fede0207ef9ab2f8720f3bddab
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-05 15:45:00 +0300

    Cleanup

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,8 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
+// automatically import all files ending in *.stories.js|jsx|ts|tsx
 const req = require.context('../stories', true, /.stories.js|jsx|ts|tsx}$/);
 function loadStories() {
-
   req.keys().forEach(filename => req(filename));
 }
 configure(loadStories, module);
