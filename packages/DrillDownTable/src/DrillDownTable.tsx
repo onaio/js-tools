b92cdbcada5f6d5893f4d3b116e07f6f600d83c7
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-06 18:50:57 +0300

    Add HOC that enhances ReactTable

--- packages/DrillDownTable/src/DrillDownTable.tsx
+++ packages/DrillDownTable/src/DrillDownTable.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
 // Import React Table
-import ReactTable from 'react-table';
+import ReactTable, { FinalState, TableProps } from 'react-table';
 import 'react-table/react-table.css';
+import { columnsFromObject } from './utils';
 
-class DrillDownTable extends React.Component<{}, {}> {
-  constructor(props: {}) {
-    super(props);
+function DrillDownTableHOC(WrappedTable: ReactTable) {
+  class ResultingTable extends React.Component<TableProps, FinalState> {
+    constructor(props: TableProps) {
+      super(props);
+    }
+
+    public getColumns() {
+      // Either use provided columns or get them from the data
+      const { data, columns } = this.props;
+      if (columns) {
+        return columns;
+      }
+      if (data) {
+        return columnsFromObject(data[0]);
+      }
+    }
+
+    public render() {
+      const newProps = {
+        columns: this.getColumns()
+      };
+      return <WrappedTable {...newProps} {...this.props} />;
+    }
   }
 
-  public render() {
-    return (
-      <div>
-        <ReactTable />
-      </div>
-    );
-  }
+  return ResultingTable;
 }
+
+const DrillDownTable = DrillDownTableHOC(ReactTable);
 
 export default DrillDownTable;
