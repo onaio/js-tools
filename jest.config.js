ad83b79382c3530acac94c08e2eb6297b6db07eb
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-06 13:24:57 +0300

    Improve jest config
    - Add TypeScript support
    - Add file object mapper

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
-    'packages/**/*.{js,jsx}',
+    'packages/**/*.{js,jsx,ts,tsx}',
     '!**/node_modules/**',
     '!**/coverage/**',
     '!**/dist/**'
   ],
   setupFiles: ['./setupTests'],
-  roots: ['packages/']
+  roots: ['packages/'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy'
+  }
 };
