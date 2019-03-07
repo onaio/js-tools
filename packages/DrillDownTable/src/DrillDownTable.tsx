7d2f06d9427c2cd9728352be7bb09f7128a8c96d
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-07 18:28:17 +0300

    Add filtering to `DrillDownTable` and QoL improvements

--- packages/DrillDownTable/src/DrillDownTable.tsx
+++ packages/DrillDownTable/src/DrillDownTable.tsx
@@ -1,26 +1,55 @@
 import React, { ComponentType } from 'react';
 import ReactTable, { TableProps } from 'react-table';
 import 'react-table/react-table.css';
+import { DataElement } from './utils';
 import WithHeaders from './WithHeaders';
+
+/** Interface to define props of Drill down table */
+interface DrillDownProps<T> extends Partial<TableProps<T>> {
+  identifierField?: string;
+  parentIdentifierField?: string;
+}
 
 /** A Higher order component that adds drill-down capability to render
  * hierarchical data in tables that allow you to move from the highest level to
  * the lowest, nad back with maximum flexibility.
  */
 export function WithDrillDown(WrappedTable: ComponentType) {
-  class TableWithDrills extends React.Component<Partial<TableProps>, {}> {
-    constructor(props: TableProps) {
+  class TableWithDrills<T extends object> extends React.Component<Partial<DrillDownProps<T>>, {}> {
+    public static defaultProps = {
+      identifierField: 'id',
+      parentIdentifierField: 'parent_id'
+    };
+
+    constructor(props: DrillDownProps<T>) {
       super(props);
     }
 
     public render() {
-      const newProps = {};
-      return <WrappedTable {...newProps} {...this.props} />;
+      const newProps = {
+        data: this.getHierarchicyData()
+      };
+      return <WrappedTable {...this.props} {...newProps} />;
+    }
+
+    private filterForLevel(element: DataElement) {
+      if (element.hasOwnProperty('parent_id')) {
+        return element.parent_id === null;
+      }
+      return false;
+    }
+
+    private getHierarchicyData() {
+      const { data } = this.props;
+      if (data) {
+        return data.filter(this.filterForLevel);
+      }
+      return data;
     }
   }
 
   return TableWithDrills;
 }
 
-const DrillDownTable = WithHeaders(ReactTable);
+const DrillDownTable = WithHeaders(WithDrillDown(ReactTable));
 export default DrillDownTable;
