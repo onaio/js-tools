6dfcb8e1cef41567b9e0233d82777a3ea4c2db37
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-07 16:23:14 +0300

    Add tests for WithHeaders

--- packages/DrillDownTable/src/tests/DrillDownTable.test.tsx
+++ packages/DrillDownTable/src/tests/DrillDownTable.test.tsx
@@ -1,6 +1,9 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import React from 'react';
-import DrillDownTable from '../DrillDownTable';
+import ReactTable from 'react-table';
+import DrillDownTable, { WithHeaders } from '../DrillDownTable';
+import { data } from './fixtures';
 
 describe('DrillDownTable', () => {
   beforeEach(() => {
@@ -9,5 +12,30 @@ describe('DrillDownTable', () => {
 
   it('renders without crashing', () => {
     shallow(<DrillDownTable />);
+  });
+});
+
+describe('DrillDownTable/WithHeaders', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    const SomeTable = WithHeaders(ReactTable);
+    shallow(<SomeTable data={data} />);
+  });
+
+  it('renders correctly with custom columns', () => {
+    const SomeTable = WithHeaders(ReactTable);
+    const wrapper = mount(<SomeTable data={data} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders correctly with derived columns', () => {
+    const SomeTable = WithHeaders(ReactTable);
+    const wrapper = mount(<SomeTable data={data} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
   });
 });
