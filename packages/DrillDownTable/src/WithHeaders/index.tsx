f74fe1a95afc91db565a9ecdd3850e685808e42b
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-13 17:01:48 +0300

    Refactor WithHeaders to be function-based for clarity and simplicity

--- packages/DrillDownTable/src/WithHeaders/index.tsx
+++ packages/DrillDownTable/src/WithHeaders/index.tsx
@@ -1,39 +1,31 @@
-import React, { ComponentType } from 'react';
-import { TableProps } from 'react-table';
+import React from 'react';
+import ReactTable, { Column, TableProps } from 'react-table';
 import 'react-table/react-table.css';
 import { columnsFromObject } from '../helpers/utils';
+
+/** Simple function to use provided columns or get them from the data */
+export function getColumns(props: Partial<TableProps>): Column[] {
+  const { data, columns } = props;
+  if (columns) {
+    return columns;
+  }
+  if (data) {
+    return columnsFromObject(data[0]);
+  }
+
+  return [];
+}
 
 /** A Higher order component that ensures table headers (column) are present
  * and are passed on to the WrappedTable component.
  * If columns already exist as a prop then nothing happens, otherwise columns
  * are derived from the data itself.
  */
-function WithHeaders(WrappedTable: ComponentType<any>) {
-  class TableWithHeaders<T extends object> extends React.Component<Partial<TableProps<T>>, {}> {
-    constructor(props: TableProps) {
-      super(props);
-    }
-
-    public getColumns() {
-      // Either use provided columns or get them from the data
-      const { data, columns } = this.props;
-      if (columns) {
-        return columns;
-      }
-      if (data) {
-        return columnsFromObject(data[0]);
-      }
-    }
-
-    public render() {
-      const newProps = {
-        columns: this.getColumns()
-      };
-      return <WrappedTable {...newProps} {...this.props} />;
-    }
-  }
-
-  return TableWithHeaders;
+function WithHeaders(props: Partial<TableProps>) {
+  const newProps = {
+    columns: getColumns(props)
+  };
+  return <ReactTable {...newProps} {...props} />;
 }
 
 export default WithHeaders;
