3004c00e467416a2ea915bf46d5ac8b73da8739e
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-17 14:22:49 +0300

    Add missing jsDoc strings for DropDownCell

--- packages/DrillDownTable/src/helpers/DropDownCell.tsx
+++ packages/DrillDownTable/src/helpers/DropDownCell.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { CARET, CLICKABLE_CSS_CLASS, LINKER_ITEM_CSS_CLASS } from './constants';
 
+/** Interface for DropDown cell props */
 export interface DropDownCellProps {
   cellValue: Node;
   hasChildren: boolean;
 }
 
+/** Component that will be rendered in drop-down table cells showing a caret
+ * that indicates if you can drill-down a row of data or not.
+ */
 const DropDownCell: React.ElementType = (props: DropDownCellProps) => {
   const { cellValue, hasChildren } = props;
   return (
