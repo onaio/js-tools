63a4a64227935019219ae4042a97c5656bebdfc8
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-19 17:52:26 +0300

    Rewrite ElementMap story in Typescript

--- stories/ElementMap/ElementMap.stories.tsx
+++ stories/ElementMap/ElementMap.stories.tsx
@@ -1,10 +1,10 @@
 // stories of ElementMap
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
 import { storiesOf } from '@storybook/react';
-/* eslint-enable import/no-extraneous-dependencies */
-import ElementMap from '../../packages/ElementMap/src/ElementMap';
+import React from 'react';
 import notes from '../../packages/ElementMap/README.md';
+/* eslint-enable import/no-extraneous-dependencies */
+import ElementMap from '../../packages/ElementMap/src/';
 
 function renderList() {
   const fruits = ['Apple', 'Banana', 'Grape', 'Orange', 'Plum'];
