6b1cd05a9fc45bbefbd2fb8bca8719b61753e58c
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-28 17:47:27 +0300

    Update dist files

--- packages/DrillDownTable/dist/helpers/DropDownCell.js
+++ packages/DrillDownTable/dist/helpers/DropDownCell.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -8,8 +10,6 @@ exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 
 var _constants = require("./constants");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var DropDownCell = function DropDownCell(props) {
   var cellValue = props.cellValue,
