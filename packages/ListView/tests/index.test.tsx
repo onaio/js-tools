969ef207b1837efc6580b5cfe6238255b2753d30
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-03-29 15:05:22 +0300

    Add more tests for ListView
    Co-authored-by: Kahama <kahama94@gmail.com>
    Co-authored-by: Kelvin <kelvin@jayanoris.com>

--- packages/ListView/tests/index.test.tsx
+++ packages/ListView/tests/index.test.tsx
@@ -1,7 +1,7 @@
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import React from 'react';
-import ListView from '../';
+import ListView, { ListViewProps } from '../';
 
 describe('ListView', () => {
   beforeEach(() => {
@@ -16,12 +16,44 @@ describe('ListView', () => {
     shallow(<ListView {...props} />);
   });
 
-  it('renders correctly with derived columns', () => {
+  it('renders correctly', () => {
     const props = {
       data: [['Data 1'], ['Data 2'], ['Data 3']],
       headerItems: ['Columns']
     };
     const wrapper = mount(<ListView {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders correctly with different types of data', () => {
+    const props: ListViewProps = {
+      data: [['Data 1'], [999], ['Data 3']],
+      headerItems: ['Columns']
+    };
+    const wrapper = mount(<ListView {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders correctly with rows contining components', () => {
+    const SomeComponent: React.ElementType = props => {
+      return (
+        <div>
+          <span>{props.value}</span>
+        </div>
+      );
+    };
+
+    const tableProps: ListViewProps = {
+      data: [
+        ['Data 1'],
+        [<div key="123">Data 2</div>],
+        [<SomeComponent key="456" value="Data 3" />]
+      ],
+      headerItems: ['Columns']
+    };
+    const wrapper = mount(<ListView {...tableProps} />);
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
   });
