b1ec923005408e377f3631387e6829d65faba42d
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-18 14:32:32 +0300

    Add fixture for Onadata user API response

---
+++ packages/gatekeeper/src/helpers/tests/fixtures.ts
@@ -0,0 +1,18 @@
+/** An example of a user from Onadata */
+export const onadataUser = {
+  api_token: 'the api token',
+  city: '',
+  country: '',
+  email: 'mosh@example.com',
+  gravatar:
+    'https://secure.gravatar.com/avatar/ae22ab897231db07205bd5d00e64cbbf?d=https%3A%2F%2Fona.io%2Fstatic%2Fimages%2Fdefault_avatar.png&s=60',
+  name: 'mosh',
+  organization: '',
+  require_auth: true,
+  temp_token: 'the temp token',
+  twitter: '',
+  url: 'https://stage-api.ona.io/api/v1/profiles/moshthepitt',
+  user: 'https://stage-api.ona.io/api/v1/users/moshthepitt',
+  username: 'moshthepitt',
+  website: ''
+};
