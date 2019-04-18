0ad531d51210cdae1228148964aa11f1f15a7fed
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-18 18:47:18 +0300

    Fix broken tests

--- packages/gatekeeper/src/helpers/tests/services.test.ts
+++ packages/gatekeeper/src/helpers/tests/services.test.ts
@@ -10,13 +10,15 @@ describe('gatekeeper/services', () => {
   });
 
   it('oauth2Callback should work', async () => {
+    const provider = fixtures.onadataAuth;
     const data = fixtures.onadataUser;
+    const url = 'https://stage-api.ona.io/api/v1/user.json';
     const hash =
       '#access_token=iLoveOov&expires_in=36000&token_type=Bearer&scope=read+write&state=abc';
 
-    fetchMock.getOnce('https://stage-api.ona.io/api/v1/user.json', JSON.stringify(data));
+    fetchMock.getOnce(url, JSON.stringify(data));
 
-    const response = await oauth2Callback(hash);
+    const response = await oauth2Callback(hash, url, provider);
 
     const expectedResponse = fixtures.onadataUser;
 
@@ -24,22 +26,28 @@ describe('gatekeeper/services', () => {
   });
 
   it('fetchUser should work', async () => {
+    const provider = fixtures.onadataAuth;
     const data = fixtures.onadataUser;
+    const url = 'https://stage-api.ona.io/api/v1/user.json';
     const hash =
       '#access_token=iLoveOov&expires_in=36000&token_type=Bearer&scope=read+write&state=abc';
     fetchMock.getOnce('https://stage-api.ona.io/api/v1/user.json', JSON.stringify(data));
 
-    // call fetchUser
+    // mock fetchUser
     const mock = jest.spyOn(sessionReducer, 'authenticateUser');
 
-    await fetchUser(hash, jest.fn());
+    await fetchUser(hash, url, jest.fn(), provider);
 
-    expect(mock).toHaveBeenCalledWith('the api token', true, {
-      email: 'mosh@example.com',
-      gravatar:
-        'https://secure.gravatar.com/avatar/ae22ab897231db07205bd5d00e64cbbf?d=https%3A%2F%2Fona.io%2Fstatic%2Fimages%2Fdefault_avatar.png&s=60',
-      name: 'mosh',
-      username: 'moshthepitt'
-    });
+    expect(mock).toHaveBeenCalledWith(
+      true,
+      {
+        email: 'mosh@example.com',
+        gravatar:
+          'https://secure.gravatar.com/avatar/ae22ab897231db07205bd5d00e64cbbf?d=https%3A%2F%2Fona.io%2Fstatic%2Fimages%2Fdefault_avatar.png&s=60',
+        name: 'mosh',
+        username: 'moshthepitt'
+      },
+      data
+    );
   });
 });
