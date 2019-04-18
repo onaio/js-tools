f3395882e5a2d8ec8092537a0b2862221e81374f
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-18 14:53:51 +0300

    Add oAuth helper functions

--- packages/gatekeeper/src/helpers/oauth.ts
+++ packages/gatekeeper/src/helpers/oauth.ts
@@ -1,10 +1,24 @@
-import ClientOAuth2 from 'client-oauth2';
+import ClientOAuth2, { Options } from 'client-oauth2';
 
-export const onadataAuth = new ClientOAuth2({
-  accessTokenUri: 'https://stage-api.ona.io/o/token/',
-  authorizationUri: 'https://stage-api.ona.io/o/authorize/',
-  clientId: 'iDWxqlTdUNUtGd3MIjxsI8HcICSjsRwHfy1LKvzF',
-  redirectUri: 'http://localhost:3000/oauth/callback/onadata/',
-  scopes: ['read', 'write'],
-  state: 'abc'
-});
+/** interface for oAuth options */
+export interface OauthOptions extends Options {
+  userUri: string;
+}
+
+/** interface for providers object */
+export interface Providers {
+  [key: string]: OauthOptions;
+}
+
+/** get oAuth provider from oAuthOptions */
+export function getProviderFromOptions(options: OauthOptions) {
+  const { accessTokenUri, authorizationUri, clientId, redirectUri, scopes, state } = options;
+  return new ClientOAuth2({
+    accessTokenUri,
+    authorizationUri,
+    clientId,
+    redirectUri,
+    scopes,
+    state
+  });
+}
