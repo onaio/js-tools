90cbf4eebf628b06024f23dcd10da337b36db882
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-18 18:46:21 +0300

    Refactor services module

--- packages/gatekeeper/src/helpers/services.ts
+++ packages/gatekeeper/src/helpers/services.ts
@@ -1,10 +1,16 @@
-import { authenticateUser } from '@onaio/session-reducer';
-import { onadataAuth } from './tests/fixtures';
+import { AuthenticateAction, authenticateUser } from '@onaio/session-reducer';
+import ClientOAuth2 from 'client-oauth2';
+import { Dispatch } from 'react';
+import { ActionCreator } from 'redux';
+import { ErrorCallback, errorCallback } from './utils';
 
-/** Calls the oAuth provider to get user details */
-export async function oauth2Callback(uri: string) {
-  return onadataAuth.token.getToken(uri).then(async (oAuthObject: any) => {
-    const url = 'https://stage-api.ona.io/api/v1/user.json';
+/** Calls the oAuth provider to get user details
+ * @param {string} locationHash - the location hash value that we receive from the oAuth provider
+ * @param {string} url - the URL that returns the user information for the provider
+ * @param {ClientOAuth2} provider - the Oauth client object for the provider
+ */
+export async function oauth2Callback(locationHash: string, url: string, provider: ClientOAuth2) {
+  return provider.token.getToken(locationHash).then(async (oAuthObject: any) => {
     const response = await fetch(
       url,
       oAuthObject.sign({
@@ -29,20 +35,31 @@ export async function oauth2Callback(uri: string) {
 
 /** This function is used to fetch the user logging in by calling oauth2Callback
  * and then calling authenticateUser to store the user in the Redux store.
+ * @param {string} locationHash - the location hash value that we receive from the oAuth provider
+ * @param {string} url - the URL that returns the user information for the provider
+ * @param {any} dispatch - the dispatch function that calls the session reducer action creator
+ * @param {ClientOAuth2} provider - the Oauth client object for the provider
+ * @param {ErrorCallback} errorCallbackFn - a function that handles error messages
  */
-export async function fetchUser(someVar: string, dispatch: any) {
+export async function fetchUser(
+  locationHash: string,
+  url: string,
+  dispatch: Dispatch<any>,
+  provider: ClientOAuth2,
+  authenticateActionCreator: ActionCreator<AuthenticateAction> = authenticateUser,
+  errorCallbackFn: ErrorCallback = errorCallback
+) {
   try {
-    const apiResponse = await oauth2Callback(someVar);
-    const { username, name, email, gravatar, api_token } = apiResponse as any;
+    const apiResponse = await oauth2Callback(locationHash, url, provider);
+    const { username, name, email, gravatar } = apiResponse as any;
     const theUser = {
       email,
       gravatar,
       name,
       username
     };
-    dispatch(authenticateUser(api_token, true, theUser));
+    dispatch(authenticateActionCreator(true, theUser, apiResponse));
   } catch (error) {
-    // ideally you dont want to throw an error here! You want to notify the user
-    throw new Error(error);
+    errorCallbackFn(error);
   }
 }
