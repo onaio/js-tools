cb8cf5779b6bf7feb2f3c456fc163c72dd3caf13
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-18 14:55:12 +0300

    Add test for getProviderFromOptions

---
+++ packages/gatekeeper/src/helpers/tests/oauth.test.ts
@@ -0,0 +1,24 @@
+import { getProviderFromOptions, Providers } from '../oauth';
+import * as fixtures from './fixtures';
+
+describe('oAuth', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('getProviderFromOptions should work', async () => {
+    const expected = fixtures.onadataAuth;
+    const providersObj: Providers = {
+      onadata: {
+        accessTokenUri: 'https://stage-api.ona.io/o/token/',
+        authorizationUri: 'https://stage-api.ona.io/o/authorize/',
+        clientId: 'iDWxqlTdUNUtGd3MIjxsI8HcICSjsRwHfy1LKvzF',
+        redirectUri: 'http://localhost:3000/oauth/callback/onadata/',
+        scopes: ['read', 'write'],
+        state: 'abc',
+        userUri: 'https://stage-api.ona.io/api/v1/user.json'
+      }
+    };
+    expect(getProviderFromOptions(providersObj.onadata)).toEqual(expected);
+  });
+});
