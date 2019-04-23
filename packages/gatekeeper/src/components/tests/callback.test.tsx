423930e3562ae2858fca1d68a38f4bc7c33f58f6
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-23 14:04:27 +0300

    Add working and enhanced tests for the callback component

--- packages/gatekeeper/src/components/tests/callback.test.tsx
+++ packages/gatekeeper/src/components/tests/callback.test.tsx
@@ -1,13 +1,22 @@
+import store from '@onaio/connected-reducer-registry';
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import session, { authenticateUser, logOutUser } from '@onaio/session-reducer';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import fetchMock from 'fetch-mock';
 import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Provider } from 'react-redux';
+import * as serviceHelpers from '../../helpers/services';
 import * as helperFixtures from '../../helpers/tests/fixtures';
-import OauthCallback from '../callback';
+import * as callback from '../callback';
 import * as fixtures from './fixtures';
 
+const ConnectedOauthCallback = callback.default;
+const OauthCallback = callback.OauthCallback;
+
 const history = createBrowserHistory();
+reducerRegistry.register('session', session);
 
 describe('gatekeeper/OauthLogin', () => {
   beforeEach(() => {
@@ -21,6 +30,8 @@ describe('gatekeeper/OauthLogin', () => {
       JSON.stringify(helperFixtures.onadataUser)
     );
     const props = {
+      authenticateUser,
+      authenticated: false,
       history,
       location: {
         hash:
@@ -35,12 +46,67 @@ describe('gatekeeper/OauthLogin', () => {
         path: 'https://example.com/oauth/callback/:id',
         url: 'https://example.com/oauth/callback/404/'
       },
-      providers: fixtures.providers
+      providers: fixtures.providers,
+      sessionData: {},
+      sessionUser: {
+        email: '',
+        gravatar: '',
+        name: '',
+        username: ''
+      }
     };
     shallow(<OauthCallback {...props} />);
   });
 
-  it('renders correctly', () => {
+  it('renders correctly when not logged in', () => {
+    fetchMock.getOnce(
+      fixtures.providers.onadata.userUri,
+      JSON.stringify(helperFixtures.onadataUser)
+    );
+    const props = {
+      authenticateUser,
+      authenticated: false,
+      history,
+      location: {
+        hash:
+          '#access_token=iLoveOov&expires_in=36000&token_type=Bearer&scope=read+write&state=abc',
+        pathname: '/oauth/callback/onadata/',
+        search: '',
+        state: undefined
+      },
+      match: {
+        isExact: true,
+        params: { id: 'onadata' },
+        path: 'https://example.com/oauth/callback/:id',
+        url: 'https://example.com/oauth/callback/onadata/'
+      },
+      providers: fixtures.providers,
+      sessionData: {},
+      sessionUser: {
+        email: '',
+        gravatar: '',
+        name: '',
+        username: ''
+      }
+    };
+
+    store.dispatch(logOutUser());
+    const mock = jest.spyOn(serviceHelpers, 'fetchUser');
+
+    const wrapper = mount(<OauthCallback {...props} />);
+
+    const { onadataAuth } = helperFixtures;
+    const url = 'https://stage-api.ona.io/api/v1/user.json';
+    const hash =
+      '#access_token=iLoveOov&expires_in=36000&token_type=Bearer&scope=read+write&state=abc';
+
+    expect(mock).toHaveBeenCalledWith(hash, url, onadataAuth, authenticateUser);
+
+    expect(toJson(wrapper.find('OauthCallback'))).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders correctly when logged in', () => {
     fetchMock.getOnce(
       fixtures.providers.onadata.userUri,
       JSON.stringify(helperFixtures.onadataUser)
@@ -62,8 +128,17 @@ describe('gatekeeper/OauthLogin', () => {
       },
       providers: fixtures.providers
     };
-    const wrapper = mount(<OauthCallback {...props} />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+
+    const { authenticated, user, extraData } = helperFixtures.onadataSession;
+    store.dispatch(authenticateUser(authenticated, user, extraData));
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <ConnectedOauthCallback {...props} />
+      </Provider>
+    );
+
+    expect(toJson(wrapper.find('OauthCallback'))).toMatchSnapshot();
     wrapper.unmount();
   });
 });
