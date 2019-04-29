8afdcf3b87e929befbcb54c77c21599fe11caa86
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-29 23:42:36 +0300

    Add dist files

---
+++ packages/connected-private-route/dist/types/index.d.ts
@@ -0,0 +1,53 @@
+/// <reference types="react" />
+import { RouteProps } from 'react-router-dom';
+/** interface for PrivateRoute props */
+interface PrivateRouteProps extends RouteProps {
+  authenticated: boolean /** is the current user authenticated */;
+  disableLoginProtection: boolean /** should we disable login protection */;
+  redirectPath: string /** redurect to this path is use if not authenticated */;
+}
+/** The PrivateRoute component
+ * This component is a simple wrapper around Route and takes all its props in
+ * addition to:
+ *  1. {bool} authenticated
+ *  2. {string} redirectPath
+ *
+ * If authenticated === true then render the component supplied
+ * Otherwise redirect to the redirectPath
+ */
+declare const PrivateRoute: {
+  (props: PrivateRouteProps): JSX.Element;
+  defaultProps: Partial<PrivateRouteProps>;
+};
+export { PrivateRoute };
+/** create connected component */
+/** The ConnectedPrivateRoute component
+ * This component is a simple wrapper around Route and takes all its props in
+ * addition to:
+ *  1. {bool} authenticated - this comes from the Redux store
+ *  2. {string} redirectPath
+ *
+ * If authenticated === true then render the component supplied
+ * Otherwise redirect to the redirectPath
+ */
+declare const ConnectedPrivateRoute: import('react-redux').ConnectedComponentClass<
+  {
+    (props: PrivateRouteProps): JSX.Element;
+    defaultProps: Partial<PrivateRouteProps>;
+  },
+  Pick<
+    PrivateRouteProps,
+    | 'disableLoginProtection'
+    | 'redirectPath'
+    | 'location'
+    | 'component'
+    | 'render'
+    | 'children'
+    | 'path'
+    | 'exact'
+    | 'sensitive'
+    | 'strict'
+  > &
+    Partial<PrivateRouteProps>
+>;
+export default ConnectedPrivateRoute;
