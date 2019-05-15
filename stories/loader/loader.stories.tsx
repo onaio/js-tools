c35a6e15c4534afc1a166b2cd54c8a6be3454441
p-netm <pmuriuki98@gmail.com>
2019-05-15 08:58:25 +0300

    add 2 more stories
    - showing usage with different colors
    - showing usage with different border style
    
    basically shows that you can custom the component via a few available props

--- stories/loader/loader.stories.tsx
+++ stories/loader/loader.stories.tsx
@@ -1,10 +1,17 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import LoaderMarkdown from '../../packages/loader/README.md';
+import notes from '../../packages/loader/README.md';
 import Loader from '../../packages/loader/src';
 
-storiesOf('Loader', module).add('with Markdown', () => <Loader />, {
-  notes: {
-    markdown: LoaderMarkdown
-  }
-});
+const colorStyle = {
+  borderColor: '#FF22EF'
+};
+
+const sizestyle = {
+  borderStyle: 'dotted'
+};
+
+storiesOf('Loader', module)
+  .add('with default settings', () => <Loader />, { notes })
+  .add('with different color', () => <Loader {...colorStyle} />, { notes })
+  .add('with different border', () => <Loader {...sizestyle} />, { notes });
