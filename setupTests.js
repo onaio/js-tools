09931986a56be2c2eb09d95b300d1a2d7d5dcfc0
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-01 12:59:25 +0300

    Add newline

--- setupTests.js
+++ setupTests.js
@@ -2,4 +2,5 @@
 // setupTests.js is loadable without additional babel configuration.
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
+
 enzyme.configure({ adapter: new Adapter() });
