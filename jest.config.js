81e11bfcd0b46f111375a6746a32f2af563c476c
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-01 13:02:19 +0300

    Add setupJest to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     '!**/dist/**',
     '!**/{tests,__tests__}/**'
   ],
-  setupFiles: ['./setupTests'],
+  setupFiles: ['./setupJest.ts', './setupTests'],
   roots: ['packages/'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
