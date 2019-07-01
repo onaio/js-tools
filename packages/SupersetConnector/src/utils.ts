726455725e72e457c10ed08a1b1c740538eed773
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-01 13:03:44 +0300

    Make provider and endpoint optional

--- packages/SupersetConnector/src/utils.ts
+++ packages/SupersetConnector/src/utils.ts
@@ -9,11 +9,11 @@ export const processData = (res: { [key: string]: any }) => {
 export interface SupersetConnectorConfig {
   credentials?: RequestCredentials /** Custom override for Fetch API 'credentials' setting */;
   base?: string /** Overrides Auth URI Basepath, requires trailing '/' */;
-  endpoint: string /** The endpoint to hit on the Superset API */;
+  endpoint?: string /** The endpoint to hit on the Superset API */;
   extraPath?: string /** url path to append when hitting the Superset API */;
   method?: string /** Specify HTTP Method (defaults to GET) */;
   mimeType?: string /** Specify mimeType for Request Headers */;
   params?: string /** Additional parameters to be appended to API Path */;
-  provider: string /** oAuth2 Provider name as a string */;
+  provider?: string /** oAuth2 Provider name as a string */;
   token: string /** oAuth2 Access Token as a string */;
 }
