f2cb7259044c19e08fa220565ae0084102dfda92
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-04 16:01:38 +0300

    Add DEFAULT_SUPERSET_PROVIDER

--- packages/SupersetConnector/src/constants.ts
+++ packages/SupersetConnector/src/constants.ts
@@ -1,2 +1,4 @@
 export const DEFAULT_SUPERSET_URL = 'http://localhost:8088/' /** Needs to have tailing '/' */;
 export type DEFAULT_SUPERSET_URL = typeof DEFAULT_SUPERSET_URL;
+export const DEFAULT_SUPERSET_PROVIDER = 'onadata';
+export type DEFAULT_SUPERSET_PROVIDER = typeof DEFAULT_SUPERSET_PROVIDER;
