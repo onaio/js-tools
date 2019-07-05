1b146578624ffa42d1441db798024f6ff0dd73bf
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-05 14:19:09 +0300

    Add test for SQL filter

--- packages/SupersetConnector/src/tests/utils.test.ts
+++ packages/SupersetConnector/src/tests/utils.test.ts
@@ -4,5 +4,19 @@ describe('superset-connector/utils', () => {
   it('getSupersetFormData should work correctly', async () => {
     // test row_limit
     expect(getSupersetFormData(2345)).toEqual({ adhoc_filters: [], row_limit: 2345 });
+
+    // test SQL filter
+    expect(
+      getSupersetFormData(1000, [{ sqlExpression: "plan_id+=+'10f9e9fa'+AND+goal_id+!=+'73'" }])
+    ).toEqual({
+      adhoc_filters: [
+        {
+          clause: 'WHERE',
+          expressionType: 'SQL',
+          sqlExpression: "plan_id+=+'10f9e9fa'+AND+goal_id+!=+'73'"
+        }
+      ],
+      row_limit: 1000
+    });
   });
 });
