619d4f18fb5d74706cc684ef1f1b2734062a6a35
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-05 15:09:01 +0300

    Add tests for apiRequest

---
+++ packages/SupersetConnector/src/tests/api.test.ts
@@ -0,0 +1,24 @@
+import { apiRequest } from '../api';
+import { getFormData } from '../utils';
+
+describe('superset-connector/api', () => {
+  it('apiRequest constructs default url correctly', async () => {
+    const request = apiRequest({ params: 'foo=bar', token: 'hhh' }, new Headers());
+    expect(request.url).toEqual('onadata?foo=bar');
+  });
+
+  it('apiRequest constructs url correctly with getFormData', async () => {
+    const request = apiRequest(
+      {
+        params: `form_data=${JSON.stringify(
+          getFormData(1000, [{ comparator: '10f9e9fa', operator: '==', subject: 'plan_id' }])
+        )}`,
+        token: 'hhh'
+      },
+      new Headers()
+    );
+    expect(request.url).toEqual(
+      'onadata?form_data=%7B%22adhoc_filters%22:[%7B%22clause%22:%22WHERE%22,%22expressionType%22:%22SIMPLE%22,%22comparator%22:%2210f9e9fa%22,%22operator%22:%22==%22,%22subject%22:%22plan_id%22%7D],%22row_limit%22:1000%7D'
+    );
+  });
+});
