5942c8b35ffca47384fb1573d8c053cef957ae3d
FrankApiyo <franklineapiyo@gmail.com>
2019-07-31 16:33:30 +0300

    Ensure super authorized state can be reset, authorized and denied

--- packages/superset-reducer/src/tests/index.test.ts
+++ packages/superset-reducer/src/tests/index.test.ts
@@ -14,6 +14,30 @@ describe('reducers/session', () => {
   });
 
   it('Should have initial state', () => {
+    // Initially authorized property of state object is set to null
+    expect(isAuthorized(store.getState())).toBe(null);
+  });
+
+  it('Should be able to authorize and reset', () => {
+    // Initially authorized property of state object is set to null
+    expect(isAuthorized(store.getState())).toBe(null);
+
+    // Call action to set authorized property of state object to true
+    store.dispatch(authorizeSuperset(true));
+
+    // Now authorized
+    expect(isAuthorized(store.getState())).toBe(true);
+
+    // Call action to set authorized to false
+    store.dispatch(authorizeSuperset(false));
+
+    // Now not authorized
+    expect(isAuthorized(store.getState())).toBe(false);
+
+    // Call action to reset authorized property of state object to null
+    store.dispatch(resetSuperset());
+
+    // Now authorized property of state object is set to null
     expect(isAuthorized(store.getState())).toBe(null);
   });
 });
