b8439584c5fe985feb5dff61a76c4be3fc66d318
FrankApiyo <franklineapiyo@gmail.com>
2019-07-31 16:08:55 +0300

    Set up boilerplate for test

---
+++ packages/superset-reducer/src/tests/index.test.ts
@@ -0,0 +1,15 @@
+import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
+import { FlushThunks } from 'redux-testkit';
+import thunk from 'redux-thunk';
+import superset, { authorizeSuperset, isAuthorized, resetSuperset } from '..';
+
+describe('reducers/session', () => {
+  let flushThunks;
+  let store: Store;
+
+  beforeEach(() => {
+    flushThunks = FlushThunks.createMiddleware();
+    store = createStore(combineReducers({ superset }), applyMiddleware(flushThunks, thunk));
+    jest.resetAllMocks();
+  });
+});
