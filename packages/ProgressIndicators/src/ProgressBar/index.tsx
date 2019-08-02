9d0f0b2fefedc3ea49e660771f50e046853de9ad
p-netm <pmuriuki98@gmail.com>
2019-08-02 12:30:32 +0300

    Refactor and fix bugs flagged by tests

---
+++ packages/ProgressIndicators/src/ProgressBar/index.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+/** Props for ProgressBar */
+interface ProgressBarProps {
+  decimalPoints: number;
+  height: string;
+  min: number;
+  max: number;
+  value: number;
+  lineColor: string;
+  lineColorThresholds?: { [key: string]: number } | undefined;
+}
+
+/** default props for ProgressBar */
+const defaultProgressBarProps: Partial<ProgressBarProps> = {
+  decimalPoints: 0,
+  height: '10px',
+  lineColor: '#0000FF',
+  lineColorThresholds: undefined,
+  max: 100,
+  min: 0,
+  value: 0
+};
+
+/** Displays configurable progress bar
+ * lineColor prop when you don't require different line colors
+ * lineColorThresholds when you have to match lineColors to certain value thresholds.
+ * LineColorThresholds will take precedence over lineColor if both are provided
+ */
+const ProgressBar = (props: ProgressBarProps) => {
+  const { decimalPoints, height, value, lineColor, lineColorThresholds } = props;
+  let backgroundColor = lineColor;
+  const max = props.max || 100;
+  const min = props.min || 0;
+  let range = max - min;
+  if (range <= 0) {
+    range = 100;
+  }
+  const decimalValue = value / range;
+  const percentValue = decimalValue * 100;
+  const percentValueString = percentValue.toFixed(decimalPoints);
+
+  // set the line color: if lineColorThresholds is not given; lineColor will be used
+  if (lineColorThresholds) {
+    // sort the color and their thresholds by the threshold value
+    const AscendingThresholds = Object.entries(lineColorThresholds).sort((e1, e2) => e1[1] - e2[1]);
+    // top to bottom check to see which color threshold is matched first by the percentValue
+    AscendingThresholds.forEach(([key, threshold]) => {
+      if (percentValue >= threshold) {
+        backgroundColor = key;
+      }
+    });
+  }
+
+  return (
+    <div className="progress" style={{ height, marginBottom: '15px' }}>
+      <div
+        className={`progress-bar`}
+        style={{
+          backgroundColor: `${backgroundColor}`,
+          height: `100%`,
+          width: `${percentValueString}%`
+        }}
+        role="progressbar"
+        aria-valuenow={percentValue}
+        aria-valuemin={min}
+        aria-valuemax={max}
+      />
+    </div>
+  );
+};
+
+ProgressBar.defaultProps = defaultProgressBarProps;
+
+export default ProgressBar;
