68e9a5ef68b49261d91beddfb2b12b7cc63fde35
p-netm <pmuriuki98@gmail.com>
2019-10-22 10:28:40 +0300

    Test routedPaginator component

--- packages/Pagination/src/Paginator/tests/routedPaginator.test.tsx
+++ packages/Pagination/src/Paginator/tests/routedPaginator.test.tsx
@@ -1,9 +1,9 @@
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import { createBrowserHistory, createLocation } from 'history';
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import { MemoryRouter, Route, Switch } from 'react-router';
-import { BrowserRouter, link, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { BasePaginator } from '../base';
 import { RoutedPaginator } from '../routedPaginator';
 
@@ -11,7 +11,12 @@ const history = createBrowserHistory();
 
 const TestComponent = props => (
   <Switch>
-    <Route exact={true} path="/records/:tablePage" component={RoutedPaginator} />
+    <Route
+      exact={true}
+      path="/records/:tablePage"
+      // tslint:disable-next-line: jsx-no-lambda
+      render={routeProps => <RoutedPaginator {...routeProps} {...props} />}
+    />
   </Switch>
 );
 
@@ -49,7 +54,7 @@ describe('packages/paginator/routedPaginator', () => {
     expect(toJson(PIWrapper.at(paginationItemNum - 2))).toMatchSnapshot('End page');
   });
 
-  it('passes corect page from url case 1', () => {
+  it('passes corect page from url', () => {
     const wrapper = mount(
       <MemoryRouter initialEntries={['/records/2']}>
         <TestComponent />
@@ -59,13 +64,42 @@ describe('packages/paginator/routedPaginator', () => {
     expect(basePaginatorWrapper.props().currentPage).toEqual(2);
   });
 
-  it('passes corect page from url case 1', () => {
+  it('renders correctly; edge case where page is gt than actual pages', () => {
+    const props = { totalRecords: 5 };
     const wrapper = mount(
       <MemoryRouter initialEntries={['/records/2']}>
-        <TestComponent />
+        <TestComponent {...props} />
       </MemoryRouter>
     );
     const basePaginatorWrapper = wrapper.find(BasePaginator);
     expect(basePaginatorWrapper.props().currentPage).toEqual(2);
+    const PIWrapper = wrapper.find('PaginationItem');
+    const paginationItemNum = PIWrapper.length;
+    expect(paginationItemNum).toEqual(5);
+    expect(PIWrapper.at(2).hasClass('active')).toBeTruthy();
+  });
+
+  it('clicking a page redirects to the correct page', () => {
+    // 5 pages
+    const props = { totalRecords: 135, pageLimit: 10 };
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/records/2']}>
+        <TestComponent {...props} />
+      </MemoryRouter>
+    );
+    const PIWrapper = wrapper.find('PaginationItem');
+    // expect(toJson(wrapper.find(BasePaginator))).toMatchSnapshot('8 !== 7')
+    expect(PIWrapper.length).toEqual(8);
+
+    // go to the 1st page
+    PIWrapper.at(2)
+      .find('PaginationLink a')
+      .simulate('click');
+
+    wrapper.update();
+
+    // inspect the pathname entry of the current location.
+    const routedPaginatorWrapper = wrapper.find(RoutedPaginator);
+    expect((routedPaginatorWrapper.props() as any).location.pathname).toEqual('/records/1');
   });
 });
