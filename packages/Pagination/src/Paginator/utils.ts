7c5ca0128cbbd36354931fa7c6116c68cfa56e3b
p-netm <pmuriuki98@gmail.com>
2019-12-09 18:59:52 +0300

    Add a sanitization util functoin

--- packages/Pagination/src/Paginator/utils.ts
+++ packages/Pagination/src/Paginator/utils.ts
@@ -46,3 +46,10 @@ export const fetchPageNumbers = (
 
   return numberedPages;
 };
+
+/** makes sure any received page number is within 1 through to totalPages
+ * @param pageNumber - the selected page number
+ * @param allPages -  the total number of pages
+ */
+export const sanitizeNumber = (pageNumber: number, allPages: number): number =>
+  Math.min(Math.max(1, pageNumber), allPages);
