4029caf9dd9929f07a69c58df0e6bb7c2aa07626
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-01-14 15:36:08 +0300

    Add OAuthExtradata to fixtures

--- packages/gatekeeper/src/helpers/tests/fixtures.ts
+++ packages/gatekeeper/src/helpers/tests/fixtures.ts
@@ -138,3 +138,39 @@ export const expressAPIResponse = {
     }
   }
 };
+
+export const OAuthExtradata = {
+  authenticated: true,
+  extraData: {
+    api_token: 'the api token',
+    city: '',
+    country: '',
+    email: 'mosh@example.com',
+    gravatar:
+      'https://secure.gravatar.com/avatar/ae22ab897231db07205bd5d00e64cbbf?d=https%3A%2F%2Fona.io%2Fstatic%2Fimages%2Fdefault_avatar.png&s=60',
+
+    name: 'mosh',
+    oAuth2Data: {
+      access_token: 'iLoveOov',
+      expires_in: '36000',
+      scope: 'read write',
+      state: 'abc',
+      token_type: 'Bearer'
+    },
+    organization: '',
+    require_auth: true,
+    temp_token: 'the temp token',
+    twitter: '',
+    url: 'https://stage-api.ona.io/api/v1/profiles/moshthepitt',
+    user: 'https://stage-api.ona.io/api/v1/users/moshthepitt',
+    username: 'moshthepitt',
+    website: ''
+  },
+  user: {
+    email: 'mosh@example.com',
+    gravatar:
+      'https://secure.gravatar.com/avatar/ae22ab897231db07205bd5d00e64cbbf?d=https%3A%2F%2Fona.io%2Fstatic%2Fimages%2Fdefault_avatar.png&s=60',
+    name: 'mosh',
+    username: 'moshthepitt'
+  }
+};
