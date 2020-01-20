05b0389851442f225f284d6016a3f078490e0c8c
p-netm <pmuriuki98@gmail.com>
2020-01-20 14:36:37 +0300

    Add headless login hook to login component
    Make strings optional props, ofcourse using the hook does not require one
    to concern themselves with overriding the propped strings, as you could
    trivially write your own in whatever language.

--- packages/gatekeeper/src/components/login.tsx
+++ packages/gatekeeper/src/components/login.tsx
@@ -1,52 +1,70 @@
 import React from 'react';
+import { OauthLoginProps } from '..';
 import { getProviderFromOptions, Providers } from '../helpers/oauth';
 
+/** types of supported authorization grant flow */
 export enum AuthorizationGrantType {
   IMPLICIT = 'IMPLICIT',
   AUTHORIZATION_CODE = 'AUTHORIZATION_CODE'
 }
 
-/** interface for OauthLogin props */
-export interface OauthLoginProps {
-  ProviderLinksComponent?: React.ElementType;
+/** describes object to pass to the OAuthHook */
+export interface OAuthLoginHookOptions {
   providers: Providers;
-
   authorizationGrant: AuthorizationGrantType;
 }
 
 /** interface for ProviderLinks props */
-export interface ProviderLinksProps {
-  providers: Providers;
+export interface ProviderLinksProps extends OAuthLoginHookOptions {
+  OAUTH_LOGIN_PROMPT?: string;
+}
 
-  authorizationGrant: AuthorizationGrantType;
+/** interface for OauthLogin props */
+export interface OauthLoginProps extends ProviderLinksProps {
+  ProviderLinksComponent?: React.ElementType;
+  NO_PROVIDERS?: string;
+}
+
+/** hook returns an object where the provider name is the key and the value is
+ * the authorizationUri,
+ * @param {OAuthLoginHookOptions} options - initial data required for processing
+ * @return {[key: string]: string} - an object
+ */
+export function useOAuthLogin(options: OAuthLoginHookOptions): { [key: string]: string } {
+  const { providers: rawProviders, authorizationGrant } = options;
+
+  /** get the ClientOAuth2 object for each provider */
+  const authorizationUris: { [key: string]: string } = {};
+  Object.entries(rawProviders).map(item => {
+    const thisProvider = getProviderFromOptions(item[1]);
+    const authorizationUri =
+      authorizationGrant === AuthorizationGrantType.IMPLICIT
+        ? thisProvider.token.getUri()
+        : authorizationGrant === AuthorizationGrantType.AUTHORIZATION_CODE
+        ? thisProvider.code.getUri()
+        : '#';
+    authorizationUris[item[0]] = authorizationUri;
+    return authorizationUris;
+  });
+
+  return authorizationUris;
 }
 
 /** This component takes providers as a prop and renders a list of links to
  * log in with those providers
  */
 export const ProviderLinks = (props: ProviderLinksProps) => {
-  const { providers, authorizationGrant } = props;
+  const { providers, authorizationGrant, OAUTH_LOGIN_PROMPT } = props;
+  const authorizationUris = useOAuthLogin({ providers, authorizationGrant });
   return (
     <div className="gatekeeper-login">
-      <p className="gatekeeper-p">Please log in with one of the following providers</p>
-      {/** loop through the providers */
-      Object.entries(providers).map(item => {
-        const thisProvider = getProviderFromOptions(
-          item[1]
-        ); /** get the ClientOAuth2 object for each provider */
+      <p className="gatekeeper-p">{OAUTH_LOGIN_PROMPT}</p>
+      {/** loop through the authorization uris */
+      Object.entries(authorizationUris).map(item => {
         return (
           /** render a link for each provider */
           <p className="gatekeeper-p item" key={item[0]}>
-            <a
-              className="gatekeeper-btn"
-              href={
-                authorizationGrant === AuthorizationGrantType.IMPLICIT
-                  ? thisProvider.token.getUri()
-                  : authorizationGrant === AuthorizationGrantType.AUTHORIZATION_CODE
-                  ? thisProvider.code.getUri()
-                  : '#'
-              }
-            >
+            <a className="gatekeeper-btn" href={item[1]}>
               {item[0]}
             </a>
           </p>
@@ -60,18 +78,27 @@ export const ProviderLinks = (props: ProviderLinksProps) => {
  * links of oAuth providers.
  */
 const OauthLogin = (props: OauthLoginProps) => {
-  const { providers, ProviderLinksComponent, authorizationGrant } = props;
+  const {
+    providers,
+    ProviderLinksComponent,
+    authorizationGrant,
+    NO_PROVIDERS,
+    OAUTH_LOGIN_PROMPT
+  } = props;
   return ProviderLinksComponent && providers ? (
-    <ProviderLinksComponent {...{ providers, authorizationGrant }} />
+    <ProviderLinksComponent {...{ providers, authorizationGrant, OAUTH_LOGIN_PROMPT }} />
   ) : (
     <div className="gatekeeper-login">
-      <p className="gatekeeper-p">No providers</p>
+      <p className="gatekeeper-p">{NO_PROVIDERS}</p>
     </div>
   );
 };
 
 OauthLogin.defaultProps = {
-  ProviderLinksComponent: ProviderLinks /** use the ProviderLinks component as the default */
+  NO_PROVIDERS: 'No Providers',
+  OAUTH_LOGIN_PROMPT: 'Please log in with one of the following providers',
+  ProviderLinksComponent: ProviderLinks /** use the ProviderLinks component as the default */,
+  authorizationGrant: AuthorizationGrantType.IMPLICIT,
 };
 
 export default OauthLogin;
