742783476a42d6b5e1c07ba8ad273ad482cf1adc
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-03-07 12:50:33 +0300

    Revert "Add percentage"
    This reverts commit f98f737367e3657744fe6717b95a265bd733fab1.

--- packages/utils/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Result } from './types';
-
-/** Returns a number as a decimal e.g. 0.18 becomes 18% */
-export function percentage(num: number, decimalPoints: number = 0): Result<string> {
-  return {
-    error: null,
-    value: `${(num * 100).toFixed(decimalPoints)}%`
-  };
-}
