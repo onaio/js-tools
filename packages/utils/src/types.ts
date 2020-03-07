6f224514d59adc137fa8f1fedb11c744b8b82d7f
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-03-07 12:38:40 +0300

    Add Success, Failure and Result

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -8,3 +8,29 @@ export interface Dictionary<T = any> {
  * to extend the base
  */
 export type UpdateType<T extends any, Y> = T & Y;
+
+/** This interface is meant to be used as the output of a function
+ * which has returned an error and no value.
+ */
+export interface Failure {
+  error: Error;
+  value: null;
+}
+
+/** This interface is meant to be used as the output of a function
+ * which has returned an value and no error.
+ */
+export interface Success<T> {
+  error: null;
+  value: T;
+}
+
+/** This is a generic interface that describes the output (or ... Result) of
+ * a function.
+ *
+ * A function can either return Success, or Failure.  The intention is to make
+ * it clear that both Success and Failure must be considered and handled.
+ *
+ * Inspired by https://gdelgado.ca/type-safe-error-handling-in-typescript.html#title
+ */
+export type Result<T> = Success<T> | Failure;
