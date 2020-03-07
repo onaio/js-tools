d1a766dd9f2880889b41f2d1f5d6e5374225de1f
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-03-07 12:30:41 +0300

    Add UpdateType

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -2,3 +2,9 @@
 export interface Dictionary<T = any> {
   [key: string]: T;
 }
+
+/** Generic Type for any object to be updated
+ *  where T is the base interface and Y is the interface
+ * to extend the base
+ */
+export type UpdateType<T extends any, Y> = T & Y;
