b093a89a28e1b6a64ca2a9eae5c4bb4935ce4614
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-03-07 13:07:05 +0300

    Improve docstring for percentage

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -1,6 +1,11 @@
 import { Result } from './types';
 
-/** Returns a number as a decimal e.g. 0.18 becomes 18% */
+/** Returns a decimal number as a percentage e.g. 0.18 becomes 18%
+ * @param num - the decimal number you wish to turn into a percentage
+ * @param decimalPoints - the number of decimal points to use
+ * @returns { Result<string> } - A result object whose value is the percentage
+ * as a string.
+ */
 export function percentage(num: number, decimalPoints: number = 0): Result<string> {
   return {
     error: null,
