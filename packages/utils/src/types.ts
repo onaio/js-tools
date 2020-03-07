73de6bde5e05e7d39c6b668c1cbf79cee953d905
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-03-07 13:04:30 +0300

    Improve docstring for Dictionary

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,4 +1,16 @@
-/** Interface for a generic object */
+/** Interface for a generic object
+ *
+ * This can work when you need to describe an object which is unknown e.g.
+ * the result of some API call.
+ *
+ * The generic type also makes it useful as an interface to an object whose
+ * properties are all of the same type e.g. {foo: 1, bar: 3} could be of type
+ * Dictionary<number>
+ *
+ * It is also easy to abuse this interface to describe any kind of object (T is
+ * any by default).  Care must be taken not to do this.  Use Dictionary only when
+ * you have to.
+ */
 export interface Dictionary<T = any> {
   [key: string]: T;
 }
