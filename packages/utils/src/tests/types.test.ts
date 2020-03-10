cbbff6c78d73c4b39ae0bfcb50c827353ffba18f
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-03-10 10:42:04 +0300

    Revert "Test for bad types"
    This reverts commit 309747c7a73a31a45dadc82ab946309e42f09ba5.
    
    Turns out that expectError does not work when you also do @ts-ignore
    because it actually wants to check that an error was raised by the
    Typescript compiler.

--- packages/utils/src/tests/types.test.ts
+++ packages/utils/src/tests/types.test.ts
@@ -1,41 +1,25 @@
-import { expectError, expectType } from 'tsd';
+import { expectType } from 'tsd';
 import { Dictionary, Failure, Result, Success, UpdateType } from '../types';
 
 describe('types', () => {
   it('Dictionary works', () => {
     expectType<Dictionary>({ bar: 'zzzz', foo: 123 });
     expectType<Dictionary<number>>({ bar: 345, foo: 123 });
-    // @ts-ignore
-    expectError<Dictionary<number>>({ bar: 'zzzz', foo: 123 });
-    // @ts-ignore
-    expectError<Dictionary>(16);
   });
 
   it('Failure works', () => {
     expectType<Failure>({ error: new Error('!'), value: null });
-    // @ts-ignore
-    expectError<Failure>({ bar: 1337 });
   });
 
   it('Success works', () => {
     expectType<Success<string>>({ error: null, value: 'mosh' });
     expectType<Success<number>>({ error: null, value: 1337 });
-    // @ts-ignore
-    expectError<Success<string>>({ error: null, value: 56 });
-    // @ts-ignore
-    expectError<Success<number>>({ value: 1337 });
   });
 
   it('Result works', () => {
     expectType<Result<string>>({ error: new Error('!'), value: null });
     expectType<Result<string>>({ error: null, value: 'mosh' });
     expectType<Result<number>>({ error: null, value: 1337 });
-    // @ts-ignore
-    expectError<Result<string>>({ error: null, value: 56 });
-    // @ts-ignore
-    expectError<Result<number>>({ value: 1337 });
-    // @ts-ignore
-    expectError<Result>({ bar: 1337 });
   });
 
   it('UpdateType works', () => {
@@ -50,7 +34,5 @@ describe('types', () => {
     type AB = UpdateType<A, B>;
 
     expectType<AB>({ bar: 1337, foo: 'leet' });
-    // @ts-ignore
-    expectError<AB>({ bar: 1337 });
   });
 });
