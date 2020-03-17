52bbe6e01b794de4aaf7949b45f4b426e184a971
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-03-17 09:05:41 +0300

    Remove   InitializeOptions interface from react-ga export

--- packages/GoogleAnalytics/src/index.tsx
+++ packages/GoogleAnalytics/src/index.tsx
@@ -1,13 +1,5 @@
-import { InitializeOptions } from 'react-ga';
 import { RouteTracker } from './components/GARoute';
 import WithGATracker from './components/WithGATracker';
 import { Dimensions, initGoogleAnalytics, setDimensions } from './helpers';
 
-export {
-  WithGATracker,
-  RouteTracker,
-  initGoogleAnalytics,
-  Dimensions,
-  InitializeOptions,
-  setDimensions
-};
+export { WithGATracker, RouteTracker, initGoogleAnalytics, Dimensions, setDimensions };
