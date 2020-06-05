9b57f4d6f87a382848e8cf6f21bc4f479e92488d
p-netm <pmuriuki98@gmail.com>
2020-06-05 11:21:45 +0300

    Export test-utils api

--- packages/DrillDownTable/src/index.tsx
+++ packages/DrillDownTable/src/index.tsx
@@ -9,3 +9,6 @@ export * from './helpers/utils';
 export * from './components/Pagination';
 export * from './components/HelperComponents';
 export * from './components/SortIcon';
+
+/** test utils */
+export * from './test-utils';
