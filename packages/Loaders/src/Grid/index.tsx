fdf22cbf7c8b339b32ddafe505b8a430f762cd74
p-netm <pmuriuki98@gmail.com>
2020-09-20 22:25:04 +0300

    Make scaleBySize more wholesome

--- packages/Loaders/src/Grid/index.tsx
+++ packages/Loaders/src/Grid/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import Styled, { css, keyframes } from 'styled-components';
-import { commonDefaultProps, CommonProps } from '../commons';
+import Styled, { keyframes } from 'styled-components';
+import { commonDefaultProps, CommonProps, getAnimationDuration } from '../commons';
+
+export const defaultAnimationDuration = 1.2;
+
+export type GridProps = CommonProps;
 
 export const gridAnimation = keyframes`
 0%, 100% {
@@ -14,16 +18,18 @@ export const gridAnimation = keyframes`
 export const StyledGrid = Styled.div`
   display: inline-block;
   position: relative;
-  width: 80px;
-  height: 80px;
+  width: ${(props: GridProps) => `${props.scaleSizeBy * 80}px`};
+  height: ${(props: GridProps) => `${props.scaleSizeBy * 80}px`};
+
 
   div {
     position: absolute;
-    width: ${(props: GripProps) => `${props.scaleSizeBy * 16}px`};
-    height: ${(props: GripProps) => `${props.scaleSizeBy * 16}px`};
+    width: ${(props: GridProps) => `${props.scaleSizeBy * 16}px`};
+    height: ${(props: GridProps) => `${props.scaleSizeBy * 16}px`};
     border-radius: 50%;
-    background: ${(props: GripProps) => props.color};
-    animation: ${gridAnimation} ${(props: GripProps) => `${props.animationSpeed}s`} linear infinite
+    background: ${(props: GridProps) => props.color};
+    animation: ${gridAnimation} ${(props: GridProps) =>
+  getAnimationDuration(props, defaultAnimationDuration)} linear infinite
   }
 
   div:nth-Child(1) {
@@ -34,57 +40,54 @@ export const StyledGrid = Styled.div`
 
   div:nth-Child(2) {
     top: 8px;
-    left: 32px;
+    left: ${(props: GridProps) => `${props.scaleSizeBy * 32}px`};
     animation-delay: -0.4s;
   }
 
   div:nth-Child(3) {
     top: 8px;
-    left: 56px;
+    left: ${(props: GridProps) => `${props.scaleSizeBy * 56}px`};
     animation-delay: -0.8s;
   }
 
   div:nth-Child(4) {
-    top: 32px;
+    top: ${(props: GridProps) => `${props.scaleSizeBy * 32}px`};
     left: 8px;
     animation-delay: -0.4s;
   }
 
   div:nth-Child(5) {
-    top: 32px;
-    left: 32px;
+    top: ${(props: GridProps) => `${props.scaleSizeBy * 32}px`};
+    left: ${(props: GridProps) => `${props.scaleSizeBy * 32}px`};
     animation-delay: -0.8s;
   }
 
   div:nth-Child(6) {
-    top: 32px;
-    left: 56px;
+    top: ${(props: GridProps) => `${props.scaleSizeBy * 32}px`};
+    left: ${(props: GridProps) => `${props.scaleSizeBy * 56}px`};
     animation-delay: -1.2s;
   }
 
   div:nth-Child(7) {
-    top: 56px;
+    top: ${(props: GridProps) => `${props.scaleSizeBy * 56}px`};
     left: 8px;
     animation-delay: -0.8s;
   }
   div:nth-Child(8) {
-    top: 56px;
-    left: 32px;
+    top: ${(props: GridProps) => `${props.scaleSizeBy * 56}px`};
+    left: ${(props: GridProps) => `${props.scaleSizeBy * 32}px`};
     animation-delay: -1.2s;
   }
 
   div:nth-Child(9) {
-    top: 56px;
-    left: 56px;
+    top: ${(props: GridProps) => `${props.scaleSizeBy * 56}px`};
+    left: ${(props: GridProps) => `${props.scaleSizeBy * 56}px`};
     animation-delay: -1.6s;
   }
 `;
 
-type GripProps = CommonProps;
-
-const defaultGridProps = {
-  ...commonDefaultProps,
-  animationSpeed: 1.2
+export const defaultGridProps = {
+  ...commonDefaultProps
 };
 
 const Grid = (props: CommonProps) => {
