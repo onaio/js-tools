04f0e3c99c54b95cb87989be385076bb9325fa09
p-netm <pmuriuki98@gmail.com>
2020-09-25 17:43:51 +0300

    Move types to own file

---
+++ packages/DrillDownTable/src/helpers/types.ts
@@ -0,0 +1,17 @@
+import { Dictionary } from '@onaio/utils';
+import { TableJSXProps } from '..';
+
+export type DrillDownFilter<D extends object> = (
+  props: DrillDownTableProps<D>,
+  parentId: string
+) => D[];
+
+/** describes props for the DrillDownTable component */
+export interface DrillDownTableProps<D extends object>
+  extends Omit<TableJSXProps<D>, 'fetchData' | 'parentNodes' | 'fullData'> {
+  extraCellProps?: Dictionary /** props to be given to CellComponent */;
+  CellComponent: React.ElementType /** The component used to render the cell that has the drill down */;
+  loading: boolean /** if loading */;
+  loadingComponent: React.ElementType /** custom component to show whilst loading is true */;
+  drillDownFilter: DrillDownFilter<D>;
+}
