4882a8f1cdf86145ac799866fae54cc79a454639
p-netm <pmuriuki98@gmail.com>
2020-09-27 21:07:23 +0300

    Update storybook

--- stories/Loaders/loaders.stories.tsx
+++ stories/Loaders/loaders.stories.tsx
@@ -2,7 +2,15 @@ import { storiesOf } from '@storybook/react';
 import React, { useState } from 'react';
 import Styled from 'styled-components';
 import notes from '../../packages/Loaders/README.md';
-import { CircleRotate, DualRing, Ellipsis, Facebook, Grid, Ring } from '../../packages/Loaders/src';
+import {
+  CircleRotate,
+  CommonProps,
+  DualRing,
+  Ellipsis,
+  Facebook,
+  Grid,
+  Ring
+} from '../../packages/Loaders/src';
 
 const CenteredContainer = Styled.div`
   width: 100vw;
@@ -22,7 +30,16 @@ const LoadersContainer = Styled.div`
 `;
 
 const FiltersGrid = Styled.div`
-  padding: 10px;
+  grid-area: filters;
+  padding: 20px 10px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const SingleFilter = Styled.div`
+  label {
+    display: block;
+  }
 `;
 
 const LoadersGrid = Styled.div`
@@ -34,7 +51,7 @@ const LoadersGrid = Styled.div`
 `;
 
 const SingleGrid = Styled.div.attrs(() => ({
-  className: 'card' // bootstrap integration
+  className: 'card' // external stylesheet integration, though once can still pass a className prop
 }))`
   display: flex;
   flex-direction: row;
@@ -44,32 +61,95 @@ const SingleGrid = Styled.div.attrs(() => ({
 
 export const LoadersGridView = () => {
   /** a customization tool for the loaders */
-  const [animationSpeed, setAnimationSpeed] = useState<number>(5);
-  const [color, setColor] = useState<string>('');
-  const [scale, setScale] = useState<number>(1);
 
+  const defaultColor: string = '#FF6347';
+  const defaultSpeed: number = 1;
+  const defaultScale: number = 1;
+
+  const [animationSpeed, setAnimationSpeed] = useState<number>(defaultSpeed);
+  const [color, setColor] = useState<string>(defaultColor);
+  const [scale, setScale] = useState<number>(defaultScale);
+
+  type InputEventHandlers = (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+  const colorInputHandler: InputEventHandlers = event => {
+    setColor(event.target.value);
+  };
+
+  const speedInputHandler: InputEventHandlers = event => {
+    setAnimationSpeed(Number(event.target.value));
+  };
+
+  const scaleInputHandler: InputEventHandlers = event => {
+    setScale(Number(event.target.value));
+  };
+
+  const props: CommonProps = {
+    color,
+    scaleSizeBy: scale,
+    scaleSpeedBy: animationSpeed
+  };
   return (
     <CenteredContainer>
       <LoadersContainer>
-        <FiltersGrid></FiltersGrid>
+        <FiltersGrid>
+          <SingleFilter>
+            <label htmlFor="colorPicker">Choose Color:</label>
+            <input
+              type="color"
+              id="colorPicker"
+              name="colorPicker"
+              defaultValue={defaultColor}
+              onChange={colorInputHandler}
+            />
+          </SingleFilter>
+
+          <SingleFilter>
+            <label htmlFor="animationSpeed">scale speed by:</label>
+            <input
+              type="range"
+              min={0.1}
+              max={2}
+              step={0.1}
+              id="animationSpeed"
+              name="animationSpeed"
+              defaultValue={defaultSpeed}
+              onChange={speedInputHandler}
+            />
+          </SingleFilter>
+
+          <SingleFilter>
+            <label htmlFor="scaleSize">scale size by:</label>
+            <input
+              type="range"
+              min={0.1}
+              max={2}
+              step={0.1}
+              id="scaleSize"
+              name="scaleSize"
+              defaultValue={defaultScale}
+              onChange={scaleInputHandler}
+            />
+          </SingleFilter>
+        </FiltersGrid>
         <LoadersGrid>
           <SingleGrid>
-            <CircleRotate />
+            <CircleRotate {...props} />
           </SingleGrid>
           <SingleGrid>
-            <Facebook />
+            <Facebook {...props} />
           </SingleGrid>
           <SingleGrid>
-            <DualRing />
+            <DualRing {...props} />
           </SingleGrid>
           <SingleGrid>
-            <Ellipsis />
+            <Ellipsis {...props} />
           </SingleGrid>
           <SingleGrid>
-            <Grid />
+            <Grid {...props} />
           </SingleGrid>
           <SingleGrid>
-            <Ring />
+            <Ring {...props} />
           </SingleGrid>
         </LoadersGrid>
       </LoadersContainer>
